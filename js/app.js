3649703cc98dbc13d756aae570c0da41ac1648fb
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-15 15:52:28 +0100

    FEAT added dom element of slide, color  style

--- js/app.js
+++ js/app.js
@@ -50,27 +50,27 @@ console.log('funziono')
 
 slideArrObjects = [
   {
-    sourceImg: './img/image1.0.jpf',
+    sourceImg: './img/image1.0.jpg',
     title: 'Fiume Vietnamita',
     description: 'Una bellissima alba sul fiume Mekong'
   },
   {
-    sourceImg: './img/image2.0.jpf',
+    sourceImg: './img/image2.0.jpg',
     title: 'Spiaggia Rocce Noosa',
     description: 'Il tramonto Australiano'
   },
   {
-    sourceImg: './img/image3.0.jpf',
+    sourceImg: './img/image3.0.jpg',
     title: 'Montagne Canadesi',
     description: 'La glaciale solitudine della montagna'
   },
   {
-    sourceImg: './img/image4.0.jpf',
+    sourceImg: './img/image4.0.jpg',
     title: 'Tramonto sulle Ande',
     description: 'Il sole tramonta sulle impervie montagne'
   },
   {
-    sourceImg: './img/image5.0.jpf',
+    sourceImg: './img/image5.0.jpg',
     title: 'Notte nella Savana',
     description: 'Gli astri danzano nella notte Africana'
   }
@@ -93,7 +93,18 @@ for (let i = 0; i < slideArrObjects.length; i++) {
   console.log( sourceImg, title, description )
   //
 
-  //RIMANENDO NEL CICLO FOR SOPRA SCRITTO,
+  //RIMANENDO NEL CICLO FOR SOPRA SCRITTO, CREO (per 5 volte) grazie al mix di createElement e template literal, gli elementi
+  //che dovremmo visualizzare nel DOM.
+
+  const carouselElement = document.getElementById('carousel')
+
+  carouselElement.innerHTML = `
+    <div class="slide">
+      <img src="${sourceImg}" alt="">
+      <h2 class="main-title">${title}</h2>
+      <p class="pic-description">${description}</p>
+    </div>
+  `
 
 }
 
@@ -107,53 +118,53 @@ for (let i = 0; i < slideArrObjects.length; i++) {
 
 //LOGICA DA NON TOCCARE, CREARE DINAMICAMENTE OGGETTI CON CLASSI ECC TRAMITE I FOR IN E GLI ARRAY DI OBJECTS
 
-// const slideElements = document.getElementsByClassName('slide') //prendo tutti gli elementi con classe slide.
-// slideElements[0].classList.add('active')
+const slideElements = document.getElementsByClassName('slide') //prendo tutti gli elementi con classe slide.
+slideElements[0].classList.add('active')
 
 
-// const leftBtnElement = document.getElementById('arrow-left')
+const leftBtnElement = document.getElementById('arrow-left')
 
-// const rightBtnElement = document.getElementById('arrow-right')
+const rightBtnElement = document.getElementById('arrow-right')
 
-// console.log(slideElements)
-// console.log(leftBtnElement, rightBtnElement)
+console.log(slideElements)
+console.log(leftBtnElement, rightBtnElement)
 
-// // FUNZIONALITA' AL CLICK DEL BOTTONE DESTRO (PER ANDARE AVANTI)
+// FUNZIONALITA' AL CLICK DEL BOTTONE DESTRO (PER ANDARE AVANTI)
 
-// rightBtnElement.addEventListener('click', function() {
-//   console.log('slide next', currentIndex)
-// //USO CONDIZIONE IF ovvero appena arriva al click 6 lei smettera' di andare avanti evitando di dare errore
+rightBtnElement.addEventListener('click', function() {
+  console.log('slide next', currentIndex)
+//USO CONDIZIONE IF ovvero appena arriva al click 6 lei smettera' di andare avanti evitando di dare errore
 
-//     //CONDIZIONE IF lunghezza dell'array -1 in modo tale da non dare erroe se vado oltre la sua LUNGHEZZA
-//   if (currentIndex < slideElements.length-1){
+    //CONDIZIONE IF lunghezza dell'array -1 in modo tale da non dare erroe se vado oltre la sua LUNGHEZZA
+  if (currentIndex < slideElements.length-1){
 
-//     //nascondere la slide attiva togliendo la classe 'active'
-//     let currentSlide = slideElements[currentIndex]
-//     currentSlide.classList.remove('active')
+    //nascondere la slide attiva togliendo la classe 'active'
+    let currentSlide = slideElements[currentIndex]
+    currentSlide.classList.remove('active')
 
-//     //incrementare l'indice
-//     currentIndex += 1
+    //incrementare l'indice
+    currentIndex += 1
 
-//     //spostare classe 'active' e mostrare la slide successiva
-//     let nextSlide = slideElements[currentIndex]
-//     nextSlide.classList.add('active')
-//   }
+    //spostare classe 'active' e mostrare la slide successiva
+    let nextSlide = slideElements[currentIndex]
+    nextSlide.classList.add('active')
+  }
 
-// })
+})
 
 
-// leftBtnElement.addEventListener('click', function() { //la slide attiva deve essere >= a 1 della slide attiva
-//   console.log('slide back', currentIndex)
+leftBtnElement.addEventListener('click', function() { //la slide attiva deve essere >= a 1 della slide attiva
+  console.log('slide back', currentIndex)
   
-//   if (currentIndex > 0){
+  if (currentIndex > 0){
 
-//     let currentSlide = slideElements[currentIndex]
-//     currentSlide.classList.remove('active')
+    let currentSlide = slideElements[currentIndex]
+    currentSlide.classList.remove('active')
 
-//     currentIndex -= 1
+    currentIndex -= 1
 
-//     let nextSlide = slideElements[currentIndex]
-//     nextSlide.classList.add('active')
-//   }
+    let nextSlide = slideElements[currentIndex]
+    nextSlide.classList.add('active')
+  }
 
-// })
+})
